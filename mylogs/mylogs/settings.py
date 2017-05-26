b5087a3d3bf7bb0701a234dd0d855f5b6f705c50
Veydh Gooljar <vgooljar@sunnygroup.com>
2017-05-26 17:56:29 -0400

    static admin files

--- mylogs/mylogs/settings.py
+++ mylogs/mylogs/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=24$y)=n-s8+ay6sw&i057+&iwkt68v0ayzl!0e%m!x21@pj-m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["logs.veydh.com"]
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
